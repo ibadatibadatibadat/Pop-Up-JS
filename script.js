5dab40affe2fa96a3827fbfad01cbb4575460ce3
Ibadat <ibadatafridi7@gmail.com>
2023-08-13 03:39:07 -0400

    Pushing To Remote Git Repository

---
+++ script.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById("pop-up-box");
+const open_popup = () => {
+  popup.classList.add("open-popup");
+};
+const close_popup = () => {
+    popup.classList.remove("open-popup");
+  };
